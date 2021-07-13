09642f4ffee8157362d8c8316dfd9df258a0c8c4
diegojoel98 <58920143+diegojoel98@users.noreply.github.com>
2021-07-12 23:33:34 -0500

    Se modifican los archivos que tienen relacion con el detalle de un producto

--- proyecto/src/controllers/productController.js
+++ proyecto/src/controllers/productController.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const date = Date.now();
+
 const productController = {
 
     detalle: function (req, res) {
-        res.render('products/product-detail');
+        let id = req.params.id;
+        let producto;
+        for (let i = 0; i < products.length; i++) {
+            if (products[i].id == id) producto = products[i];
+        }
+        res.render('products/product-detail', { producto: producto });
     }
 
 };
--- proyecto/src/routes/product.js
+++ proyecto/src/routes/product.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+// Controlador de productos
 const productController = require('../controllers/productController');
 
-// Rutas
-router.get('/product-detail', productController.detalle);
+/* Rutas */
+
+// listado de todos los productos
+router.get('/');
+
+// obtener el formulario de creación de productos
+router.get('/create');
+
+// acción de creación, donde se envía el formulario de creación de productos
+router.post('/');
+
+// detalle de un producto
+router.get('/product-detail/:id', productController.detalle);
+
+// formulario de edición de productos
+router.get('/:id/edit');
+
+// acción de edición, donde se envía el formulario de edición de productos
+router.put('/:id');
+
+// acción de borrado de un producto
+router.delete('/:id');
 
 module.exports = router;
