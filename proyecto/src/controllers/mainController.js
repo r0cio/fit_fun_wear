49849b987078c31788040ad4d12cf913f2cc914b
letylu <letyludigital@gmail.com>
2021-07-16 03:35:32 +0200

    Se agregó la lectura del archivo products.json al controlador y la vista de main/index.ejs

--- proyecto/src/controllers/mainController.js
+++ proyecto/src/controllers/mainController.js
@@ -1,7 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 const mainController = {
 
     index: function (req, res) {
-        res.render('main/index');
+        // se leen los productos del archivo json
+        products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+		// Productos visitados
+		let productosTendencia = products.filter( (producto) => {
+			return producto.categoria == "tendencias";
+		});
+
+		// productos en oferta
+		let productosOferta = products.filter( (producto) => {
+			return producto.categoria == "ofertas";
+		})
+
+		// se renderiza la vista con esos elementos
+		res.render("main/index", { productosOferta, productosTendencia, toThousand});
+        //res.render('main/index');
     },
 
     privacidad: function (req, res) {
