a17209fa3ed68f546e9c9fd4a9dadbd1ab8bc2aa
diegojoel98 <58920143+diegojoel98@users.noreply.github.com>
2021-08-15 13:04:11 -0500

    Se completan la funcionalidad del login

--- proyecto/src/controllers/userController.js
+++ proyecto/src/controllers/userController.js
@@ -1,5 +1,5 @@
 // ************ Express validator Require ************
-const {validationResult} = require("express-validator");
+const { validationResult } = require("express-validator");
 const bycrypt = require('bcryptjs');
 const fs = require('fs');
 const path = require('path');
@@ -20,24 +20,24 @@ const userController = {
     loginProcess: (req, res) => {
         const resultValidation = validationResult(req);
         // Verifica si hay errores
-		if( resultValidation.errors.length > 0){
-			return res.render('user/login', {
-				errors: resultValidation.mapped(),
-				oldData: req.body,
-			});
-		}
+        if (resultValidation.errors.length > 0) {
+            return res.render('user/login', {
+                errors: resultValidation.mapped(),
+                oldData: req.body,
+            });
+        }
         // No hay errores
-        let userToLogin = User.findByField('email', req.body.email);        
-        if( userToLogin) {
+        let userToLogin = User.findByField('email', req.body.email);
+        if (userToLogin) {
             let correctPassword = bycrypt.compareSync(req.body.password, userToLogin.password);
             if (correctPassword) {
                 delete userToLogin.password;
                 req.session.userLogged = userToLogin;
-                if( req.body.recordame){
-                    res.cookie("userEmail", req.body.email, {maxAge: 1000 * 60});
+                if (req.body.recordarme) {
+                    res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 });
                 }
                 return res.redirect('/user/profile');
-            }  
+            }
             return res.render('user/login', {
                 errors: {
                     email: {
@@ -46,8 +46,8 @@ const userController = {
                     password: {
                         msg: 'Los datos no son correctos'
                     }
-                }               
-            });          
+                }
+            });
         }
         return res.render('user/login', {
             errors: {
@@ -60,35 +60,35 @@ const userController = {
 
     },
 
-    register: function (req , res ) {
+    register: function (req, res) {
         res.render('user/register');
     },
 
     store: function (req, res) {
-		// Se obtienen las validaciones de los campos del formulario
-        
-		const resultValidation = validationResult(req);
+        // Se obtienen las validaciones de los campos del formulario
+
+        const resultValidation = validationResult(req);
         // Verifica si hay errores
-		if( resultValidation.errors.length > 0){
-			return res.render('user/register', {
-				errors: resultValidation.mapped(),
-				oldData: req.body,
-			});
-		}
+        if (resultValidation.errors.length > 0) {
+            return res.render('user/register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body,
+            });
+        }
         // No hay errores
         // Verifica si el usuario ya está logueado
 
         let userInDb = User.findByField('email', req.body.email);
 
-        if( userInDb) {
+        if (userInDb) {
             return res.render('user/register', {
-				errors: {
+                errors: {
                     email: {
                         msg: 'Este email ya está registrado'
                     }
                 },
-				oldData: req.body,
-			});
+                oldData: req.body,
+            });
         }
         let userToCreate = {}
         if (req.file == undefined) { // Sino se agrega una imagen de perfil se agrega una por default.
@@ -109,7 +109,7 @@ const userController = {
         }
 
         let userCreated = User.create(userToCreate);
-		return res.render('user/login', { msgSuccess: 'Te has registrado con éxito'});
+        return res.render('user/login', { msgSuccess: 'Te has registrado con éxito' });
     },
 
     resetPassword: function (req, res) {
@@ -117,6 +117,7 @@ const userController = {
     },
 
     logout: function (req, res) {
+        res.clearCookie('userEmail');
         req.session.destroy();
         return res.redirect('/');
     },
--- proyecto/src/middlewares/userLoggedMiddleware.js
+++ proyecto/src/middlewares/userLoggedMiddleware.js
@@ -3,12 +3,12 @@ const user = require("../models/User");
 function userLoggedMiddleware(req, res, next) {
     res.locals.isLogged = false;
 
-    if(req.cookies.userEmail){
-        res.session.userLogged = user.findByField("email",req.cookies.userEmail);
+    if (req.cookies.userEmail) {
+        req.session.userLogged = user.findByField("email", req.cookies.userEmail);
     }
     if (req.session && req.session.userLogged) {
         res.locals.isLogged = true;
-        res.locals.userLogged = req.session.userLogged;                            
+        res.locals.userLogged = req.session.userLogged;
     }
     next();
 }
