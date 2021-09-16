72d5b74a8caab0686a9a5d59048f12784b060fa3
diegojoel98 <58920143+diegojoel98@users.noreply.github.com>
2021-09-16 10:30:24 -0500

    Se agregan el archivo de creacion de la db structure.sql, el archivo de insercion de datos data.sql y el diagrama ER en la carpeta db

---
+++ db/data.sql
@@ -0,0 +1,102 @@
+-- Inserción de datos en las tablas de la DB fitfunwear
+USE `fitFunWear`;
+
+INSERT INTO `fitFunWear`.`Rol`
+(`id_role`, `name`)
+VALUES
+(1, 'Administrador'),
+(2, 'Usuario');
+
+-- Insertando datos en tabla users
+INSERT INTO `fitFunWear`.`users`
+(`id_user`, `name`, `last_name`, `email`, `password`, `image`, `role_id`)
+VALUES
+(1, 'Leticia', 'Luna', 'lety@prueba.com',  'passwordkeylety', 'default-image.png', 1),
+(2, 'Diego', 'Gongora', 'diego@prueba.com', 'passwordkeydiego', 'default-image.png', 1),
+(3, 'Quetzalli', 'Pinzón', 'quetzalli@prueba.com', 'passwordkeyquetzalli', 'default-image.png', 1),
+(4, 'Abigail', 'Montes', 'abigail@prueba.com', 'passwordkeyabigail', 'default-image.png', 2),
+(5, 'Bruno', 'Martínez', 'bruno@prueba.com', 'passwordkeybruno', 'default-image.png', 2),
+(6, 'Diego', 'Contreras', 'diegocontreras@prueba.com', 'passwordkeydiegoc', 'default-image.png', 2),
+(7, 'Karen', 'Vargas', 'karen@prueba.com', 'passwordkeykaren', 'default-image.png', 2),
+(8, 'Montserrath', 'Alpizar', 'montse@prueba.com', 'passwordkeymontse', 'default-image.png', 2),
+(9, 'Andrea', 'Santos', 'andrea@prueba.com', 'passwordkeyandrea', 'default-image.png', 2),
+(10, 'Juan', 'Cruz', 'juan@prueba.com', 'passwordkeyjuan', 'default-image.png', 2);
+
+-- Insertando datos en tabla products
+INSERT INTO `fitFunWear`.`products`
+(`id_product`, `name`, `description`, `model`)
+VALUES
+(1, 'Tenis NIKE', 'Tenis marca NIKE', '2345'),
+(2, 'Leggings Reebok', 'Leggings marca REEBOK', '745'),
+(3, 'Conjunto FILA', 'Conjunto de short y playera marca FILA', '8345'),
+(4, 'Playera Adidas', 'Playera marca ADIDAS', '1345'),
+(5, 'Conjunto deportivo', 'Conjunto deportivo de licra, top con copas desmontables y legging a la cintura','2345');
+
+
+-- Insertando datos en tabla sizes
+INSERT INTO `fitFunWear`.`sizes`
+(`id_size`, `name`)
+VALUES
+(1, 'S'),
+(2, 'M'),
+(3, 'L'),
+(4, '21'),
+(5, '27'),
+(6, '29');
+
+-- Insertando datos en tabla colors
+INSERT INTO `fitFunWear`.`colors`
+(`id_color`, `name`)
+VALUES
+(1,'Blanco'),
+(2,'Gris'),
+(3,'Azul'),
+(4,'Verde'),
+(5,'Rosa'),
+(6,'Lila'),
+(7,'Amarillo'),
+(8,'Naranja'),
+(9,'Negro'),
+(10,'Rojo');
+
+-- Insertando datos en tabla categories
+INSERT INTO `fitFunWear`.`categories`
+(`id_category`, `name`)
+VALUES
+(1, 'Ofertas'),
+(2, 'Más vendidos'),
+(3, 'Tendencia'),
+(4, 'Normal');
+
+-- Insertando datos en tabla atributos
+INSERT INTO `fitFunWear`.`attributes`
+(`id_attribute`, `available`, `image`,`price`, `discount`, `gender`, `quantity`, `size_id`, `color_id`, `category_id`, `product_id`)
+VALUES
+(1,1,'jade-tennis.jpg', 2555.00, 10, 'H', 5, 4, 4,1,1),
+(2,1,'jade-tennis.jpg', 2555.00, 10,'H', 5, 5, 4,1,1),
+(3,1,'jade-tennis.jpg', 2555.00, 0, 'H', 10, 6, 4,4,1),
+(4,1,'tenis-lila.jpeg', 2555.00, 10,'M', 5, 4, 6,1,1),
+(5,1,'tenis-lila.jpeg', 2555.00, 10,'M', 5, 5, 6,1,1),
+(6,1,'tenis-lila.jpeg', 2555.00, 0, 'M',10, 6, 6,4,1),
+(7,1,'leggings-gris.jpeg', 550.00, 0, 'M',3, 1, 2,4,2),
+(8,1,'leggings-gris.jpeg', 550.00, 10, 'M',10, 3, 2,1,2),
+(9,1,'dusan-jovic-uTlMDzkAz-M-unsplash.jpg', 550.00, 0, 'M',3, 2, 5,4,2),
+(10,1,'dusan-jovic-uTlMDzkAz-M-unsplash.jpg', 550.00, 10, 'M',10, 3, 5,4,2),
+(11,1,'leggings-pink.jpeg', 550.00, 0, 'M',4, 3, 7,3,2),
+(12,1,'conjuntoFila.jpg', 1750.00, 0, 'H',7, 3, 3,4,3),
+(13,1,'conjuntoFila.jpg', 1750.00, 0, 'H',7, 2, 3,4,3),
+(14,1,'conjuntoFila.jpg', 1750.00, 0, 'H',7, 1, 3,4,3),
+(15,1,'conjunto-short-playera-gris.jpeg', 1750.00, 0, 'H',3, 3, 2,4,3),
+(16,1,'conjunto-short-playera-gris.jpeg', 1750.00, 0, 'H',3, 2, 2,4,3),
+(17,1,'conjunto-short-playera-gris.jpeg', 1750.00, 0, 'H',3, 1, 2,4,3),
+(18,1,'conjunto-short-playera-verde.jpeg', 1750.00, 0, 'H',4, 3, 4,4,3),
+(19,1,'conjunto-short-playera-verde.jpeg', 1750.00, 0, 'H',4, 2, 4,4,3),
+(20,1,'conjunto-short-playera-verde.jpeg', 1750.00, 0, 'H',4, 1, 4,4,3);
+
+-- Insertando datos en tabla carts
+INSERT INTO `fitFunWear`.`carts`
+(`user_id`, `quantity`, `attribute_id`)
+VALUES
+(1,1,1),
+(2,1,4),
+(3,5,3);
--- db/structure.sql
+++ db/structure.sql
@@ -1,12 +1,15 @@
+-- -----------------------------------------------------
+-- Database `fitFunWear`
+-- -----------------------------------------------------
 CREATE DATABASE IF NOT EXISTS `fitFunWear` DEFAULT CHARACTER SET utf8 ;
 USE `fitFunWear` ;
 
 -- -----------------------------------------------------
--- Table `fitFunWear`.`Rol`
+-- Table `fitFunWear`.`rol`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `fitFunWear`.`Rol` ;
+DROP TABLE IF EXISTS `fitFunWear`.`rol` ;
 
-CREATE TABLE IF NOT EXISTS `fitFunWear`.`Rol` (
+CREATE TABLE IF NOT EXISTS `fitFunWear`.`rol` (
   `id_role` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_role`))
@@ -25,10 +28,12 @@ CREATE TABLE IF NOT EXISTS `fitFunWear`.`users` (
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `image` VARCHAR(45) NOT NULL DEFAULT 'default-image,png',
+  `created_at` DATE NULL DEFAULT NULL,
+  `updated_at` DATE NULL DEFAULT NULL,
   `role_id` INT NOT NULL,
   PRIMARY KEY (`id_user`),
-  UNIQUE INDEX `email_UNIQUE` (`email`) ,
-  INDEX `fk_Users_Rol_idx` (`role_id`) ,
+  UNIQUE INDEX `email_UNIQUE` (`email`),
+  INDEX `fk_Users_Rol_idx` (`role_id`),
   CONSTRAINT `fk_Users_Rol`
     FOREIGN KEY (`role_id`)
     REFERENCES `fitFunWear`.`Rol` (`id_role`)
@@ -43,16 +48,98 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `fitFunWear`.`products` ;
 
 CREATE TABLE IF NOT EXISTS `fitFunWear`.`products` (
-  `id_product` INT NOT NULL,
+  `id_product` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NOT NULL,
-  `image` VARCHAR(45) NOT NULL DEFAULT 'default-image.png',
-  `available` TINYINT(1) NOT NULL DEFAULT 1,
+  `description` TEXT NOT NULL,
   `model` VARCHAR(45) NOT NULL,
-  `price` DECIMAL NOT NULL,
-  `discount` INT NULL DEFAULT 0,
-  `quantity` INT NULL,
+  `created_at` DATE NULL DEFAULT NULL,
+  `updated_at` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`id_product`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fitFunWear`.`sizes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fitFunWear`.`sizes` ;
+
+CREATE TABLE IF NOT EXISTS `fitFunWear`.`sizes` (
+  `id_size` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_size`),
+  UNIQUE INDEX `id_size_UNIQUE` (`id_size`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fitFunWear`.`colors`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fitFunWear`.`colors` ;
+
+CREATE TABLE IF NOT EXISTS `fitFunWear`.`colors` (
+  `id_color` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_color`),
+  UNIQUE INDEX `id_option_value_UNIQUE` (`id_color`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fitFunWear`.`categories`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fitFunWear`.`categories` ;
+
+CREATE TABLE IF NOT EXISTS `fitFunWear`.`categories` (
+  `id_category` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_category`),
+  UNIQUE INDEX `id_category_UNIQUE` (`id_category`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fitFunWear`.`attributes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fitFunWear`.`attributes` ;
+
+CREATE TABLE IF NOT EXISTS `fitFunWear`.`attributes` (
+  `id_attribute` INT NOT NULL AUTO_INCREMENT,
+  `available` TINYINT(1) NOT NULL,
+  `image` VARCHAR(45) NOT NULL DEFAULT 'default-image.png',
+  `price` DECIMAL NOT NULL,
+  `discount` TINYINT(100) NOT NULL,
+  `quantity` SMALLINT NULL,
+  `gender` ENUM('H', 'M', 'U') NOT NULL,
+  `size_id` INT NOT NULL,
+  `color_id` INT NOT NULL,
+  `category_id` INT NULL,
+  `product_id` INT NOT NULL,
+  PRIMARY KEY (`id_attribute`),
+  UNIQUE INDEX `id_attribute_UNIQUE` (`id_attribute`) ,
+  INDEX `fk_attribute_color1_idx` (`color_id`),
+  INDEX `fk_attribute_category1_idx` (`category_id`),
+  INDEX `fk_attribute_size1_idx` (`size_id`),
+  INDEX `fk_attribute_product1_idx` (`product_id`),
+  CONSTRAINT `fk_attribute_size1`
+    FOREIGN KEY (`size_id`)
+    REFERENCES `fitFunWear`.`sizes` (`id_size`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attribute_color1`
+    FOREIGN KEY (`color_id`)
+    REFERENCES `fitFunWear`.`colors` (`id_color`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attribute_category1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `fitFunWear`.`categories` (`id_category`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_attribute_product1`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `fitFunWear`.`products` (`id_product`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -63,57 +150,19 @@ DROP TABLE IF EXISTS `fitFunWear`.`carts` ;
 
 CREATE TABLE IF NOT EXISTS `fitFunWear`.`carts` (
   `user_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
   `quantity` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `product_id`),
-  INDEX `product_id_idx` (`product_id`) ,
-  INDEX `user_id_idx` (`user_id`) ,
+  `attribute_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `attribute_id`),
+  INDEX `user_id_idx` (`user_id`),
+  INDEX `fk_carts_attribute1_idx` (`attribute_id`),
   CONSTRAINT `user_id`
     FOREIGN KEY (`user_id`)
     REFERENCES `fitFunWear`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `product_id`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `fitFunWear`.`products` (`id_product`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `fitFunWear`.`options`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `fitFunWear`.`options` ;
-
-CREATE TABLE IF NOT EXISTS `fitFunWear`.`options` (
-  `id_option` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `product_id` INT NOT NULL,
-  PRIMARY KEY (`id_option`, `product_id`),
-  INDEX `fk_options_products1_idx` (`product_id`) ,
-  CONSTRAINT `fk_options_products1`
-    FOREIGN KEY (`product_id`)
-    REFERENCES `fitFunWear`.`products` (`id_product`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `fitFunWear`.`options_values`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `fitFunWear`.`options_values` ;
-
-CREATE TABLE IF NOT EXISTS `fitFunWear`.`options_values` (
-  `id_option_value` INT NOT NULL,
-  `option_id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_option_value`, `option_id`),
-  INDEX `option_id_idx` (`option_id`) ,
-  CONSTRAINT `option_id`
-    FOREIGN KEY (`option_id`)
-    REFERENCES `fitFunWear`.`options` (`id_option`)
+  CONSTRAINT `fk_carts_attribute1`
+    FOREIGN KEY (`attribute_id`)
+    REFERENCES `fitFunWear`.`attributes` (`id_attribute`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
