13a2fac2b7e9e05d4f605b6e3ba74df02b433ad1
diegojoel98 <58920143+diegojoel98@users.noreply.github.com>
2021-10-13 10:55:47 -0500

    Se mejoran las validaciones en los formularios de atributos

--- proyecto/public/js/validatorAddEditAttributes.js
+++ proyecto/public/js/validatorAddEditAttributes.js
@@ -1,6 +1,6 @@
 /* Validaciones en los formularios */
 window.addEventListener('load', function () {
-    
+
     // Validaciones en formulario añadir atributos  
     // Se capturan los elementos
     let categoria = document.querySelector('.add-attribute-categoria');
@@ -11,9 +11,11 @@ window.addEventListener('load', function () {
     let errorTalla = document.querySelector('.add-attribute-error-talla');
     let precio = document.querySelector('.add-attribute-precio');
     let errorPrecio = document.querySelector('.add-attribute-error-precio');
+    let descuento = document.querySelector('.add-attribute-descuento');
+    let errorDescuento = document.querySelector('.add-attribute-error-descuento');
     let cantidad = document.querySelector('.add-attribute-cantidad');
     let errorCantidad = document.querySelector('.add-attribute-error-cantidad');
-    
+
     // Validación select categoria con el evento click
     categoria.addEventListener('click', function () {
         if (categoria.value != '') { // si ya se selecciono una categoria
@@ -25,7 +27,7 @@ window.addEventListener('load', function () {
             }
         } else {
             categoria.classList.add('border-danger');
-            errorCategoria.innerHTML = "Debes seleccionar un categoria";
+            errorCategoria.innerHTML = "Elige una categoria";
             errorCategoria.classList.remove('d-none');
             errorCategoria.classList.add('d-block');
         }
@@ -43,12 +45,12 @@ window.addEventListener('load', function () {
     let errorDisponible = document.querySelector('.add-attribute-error-disponible');
 
     if (color || talla) {
-        
+
         // Validación select color con el evento click
         color.addEventListener('click', function () {
 
             if (generoH.checked == false && generoM.checked == false && generoU.checked == false) {
-                errorGenero.innerHTML = "Elige un género";
+                errorGenero.innerHTML = "No seleccionaste un género";
                 errorGenero.classList.remove('d-none');
                 errorGenero.classList.add('d-block');
             } else {
@@ -59,7 +61,7 @@ window.addEventListener('load', function () {
             }
 
             if (disponibleSi.checked == false && disponibleNo.checked == false) {
-                errorDisponible.innerHTML = "Debes especificar si el producto estará disponible o no";
+                errorDisponible.innerHTML = "No especificaste si el producto estará disponible o no";
                 errorDisponible.classList.remove('d-none');
                 errorDisponible.classList.add('d-block');
             } else {
@@ -78,7 +80,7 @@ window.addEventListener('load', function () {
                 }
             } else {
                 color.classList.add('border-danger');
-                errorColor.innerHTML = "Debes seleccionar un color";
+                errorColor.innerHTML = "No seleccionaste un color";
                 errorColor.classList.remove('d-none');
                 errorColor.classList.add('d-block');
             }
@@ -119,19 +121,19 @@ window.addEventListener('load', function () {
                 }
             } else {
                 talla.classList.add('border-danger');
-                errorTalla.innerHTML = "Debes seleccionar una talla";
+                errorTalla.innerHTML = "No seleccionaste una talla";
                 errorTalla.classList.remove('d-none');
                 errorTalla.classList.add('d-block');
             }
         })
 
     }
-    
+
     // Validación precio del producto
     precio.addEventListener('click', function () {
         if (precio.value == '') {
             precio.classList.add('border-danger');
-            errorPrecio.innerHTML = "Debes especificar un precio";
+            errorPrecio.innerHTML = "Escribe el precio del producto";
             errorPrecio.classList.remove('d-none');
             errorPrecio.classList.add('d-block');
         } else {
@@ -142,7 +144,12 @@ window.addEventListener('load', function () {
     precio.addEventListener('input', function () {
         if (precio.value == '') {
             precio.classList.add('border-danger');
-            errorPrecio.innerHTML = "Debes especificar un precio";
+            errorPrecio.innerHTML = "Escribe el precio del producto";
+            errorPrecio.classList.remove('d-none');
+            errorPrecio.classList.add('d-block');
+        } if (precio.value <= 0) {
+            precio.classList.add('border-danger');
+            errorPrecio.innerHTML = "Escribe un precio de producto que sea mayor que cero";
             errorPrecio.classList.remove('d-none');
             errorPrecio.classList.add('d-block');
         } else {
@@ -153,11 +160,30 @@ window.addEventListener('load', function () {
         }
     })
 
+    // Validacion descuento del producto
+    descuento.addEventListener('input', function () {
+        if (descuento.value.length >= 3 || descuento.value == '') {            
+            descuento.classList.add('border-danger');
+            errorDescuento.innerHTML = "Escribe un descuento de menos del 100%";
+            errorDescuento.classList.remove('d-none');
+            errorDescuento.classList.add('d-block');
+        } else if (descuento.value < 0) {            
+            descuento.classList.add('border-danger');
+            errorDescuento.innerHTML = "Escribe un descuento positivo";
+            errorDescuento.classList.remove('d-none');
+            errorDescuento.classList.add('d-block');
+        } else {
+            descuento.classList.remove('border-danger');            
+            errorDescuento.classList.add('d-none');
+            errorDescuento.classList.remove('d-block');
+        }
+    })
+
     // Validación cantidad del producto
     cantidad.addEventListener('click', function () {
         if (cantidad.value == '') {
             cantidad.classList.add('border-danger');
-            errorCantidad.innerHTML = "Debes especificar una cantidad de productos";
+            errorCantidad.innerHTML = "Escribe la cantidad de productos";
             errorCantidad.classList.remove('d-none');
             errorCantidad.classList.add('d-block');
         } else {
@@ -168,7 +194,12 @@ window.addEventListener('load', function () {
     cantidad.addEventListener('input', function () {
         if (cantidad.value == '') {
             cantidad.classList.add('border-danger');
-            errorCantidad.innerHTML = "Debes especificar una cantidad de productos";
+            errorCantidad.innerHTML = "Escribe la cantidad de productos";
+            errorCantidad.classList.remove('d-none');
+            errorCantidad.classList.add('d-block');
+        } if (cantidad.value <= 0) {
+            cantidad.classList.add('border-danger');
+            errorCantidad.innerHTML = "Escribe una cantidad de producto que sea mayor que cero";
             errorCantidad.classList.remove('d-none');
             errorCantidad.classList.add('d-block');
         } else {
